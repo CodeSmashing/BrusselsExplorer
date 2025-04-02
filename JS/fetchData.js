ab49c48b3358d4e8cf8eb0b5d768b90bc74de09d
NielsTanghe1 <57326188+NielsTanghe1@users.noreply.github.com>
2025-04-02 11:56:15 +0200

    Api call public toilets + table

--- JS/fetchData.js
+++ JS/fetchData.js
@@ -1,9 +1,33 @@
 'use stict';
 
-console.log('test');
+let toiletData = [];
+let searchResults = document.getElementById('search-results');
 
-fetch('https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/toilettes_publiques_vbx/records?limit=20')
-    .then(response => () => {console.log(response.json())})
+fetch('https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/toilettes_publiques_vbx/records?limit=50')
+    .then(response => response.json())
+    .then(data  => {
+        toiletData = data.results;        
+        //console.log(toiletData); //Bestaat, geopont, location, pricing, openinghours
+        toiletData.forEach(toilet => {
+            let tr = document.createElement('tr');
+            let td = document.createElement('td');
+            //location
+            td.textContent = toilet.location;
+            tr.appendChild(td.cloneNode(true));
+            //pricing
+            td.textContent = toilet.pricing_en;
+            tr.appendChild(td.cloneNode(true));
+            //opening hours
+            td.textContent = toilet.openinghours;
+            tr.appendChild(td.cloneNode(true));
+            //excists
+            td.textContent = toilet.status;
+            tr.appendChild(td.cloneNode(true));
+
+            searchResults.appendChild(tr);
+
+        });
+    })
     .catch(error => {
         console.log(`Er ging iets mis: ${error.message}`); 
     });
