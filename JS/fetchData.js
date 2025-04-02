e82f15ca143a5741981d638ca72dfca613700570
NielsTanghe1 <57326188+NielsTanghe1@users.noreply.github.com>
2025-04-02 11:20:33 +0200

    Edited api call

--- JS/fetchData.js
+++ JS/fetchData.js
@@ -1,2 +1,9 @@
 'use stict';
 
+console.log('test');
+
+fetch('https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/toilettes_publiques_vbx/records?limit=20')
+    .then(response => () => {console.log(response.json())})
+    .catch(error => {
+        console.log(`Er ging iets mis: ${error.message}`); 
+    });
