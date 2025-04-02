036c0e7f980f39d07387928f3b97e291914fce11
NielsTanghe1 <57326188+NielsTanghe1@users.noreply.github.com>
2025-04-02 16:02:52 +0200

    Search and filters + CSS update
    index.html:
      • Spelling correction
      • Add filter option checkboxes and labels
      • Add table caption
      • Moved in-document javascript to fetchData.js
    
    style.css:
      • Moved background color of body to the ::before pseudo element
      • Add background image as a cover for the <body>
      • Add text shadow to headings
      • Add display rules for table elements
      • Add maximum height to the #search-container
      • Add checkbox specific styling
      • Add .filter-item styling
      • Add overflow-y: scroll to #search-results
    
    /IMG:
      • Add background bg.jpg
    
    fetchData.js:
      • Add fetchData() function for fetching data from a provided url requiring parameters
      • Add handleInput() function to handle various inputs like:
    Inputting text in the search input field and presseng enter;
    Pressing the search input button;
    The checking and unchecking of filter options;
    • Add applyFilters() function for applying various filters to our data when needed/required
    • Add displayResults() function for appending our data to a table in our results section
    • Add toggleAside() for toggling the aside element when the aside return button is pressed
    • Add relevant variable declarations
    
    Co-Authored-By: CodeSmashing <123628904+codesmashing@users.noreply.github.com>

--- JS/fetchData.js
+++ JS/fetchData.js
@@ -3,12 +3,41 @@
 let toiletData = [];
 let searchResults = document.querySelector('tbody');
 
-fetch('https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/toilettes_publiques_vbx/records?limit=50')
-    .then(response => response.json())
-    .then(data  => {
-        toiletData = data.results;        
-        //console.log(toiletData); //Bestaat, geopont, location, pricing, openinghours
-        toiletData.forEach(toilet => {
+const urlDatasetPublicToilets = 'https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/toilettes_publiques_vbx/records?limit={LIMIT}';
+const aside = document.querySelector("aside");
+const asideReturnButton = aside.querySelector(".return");
+const searchContainer = document.querySelector("#search-container");
+const searchInputField = searchContainer.querySelector("#search-input");
+const searchInputButton = searchContainer.querySelector("#search-button");
+const searchFilterContainer = searchContainer.querySelector("#search-filter-container");
+const filterPrice = searchFilterContainer.querySelector("#filter-price");
+// const filterAvailability = searchFilterContainer.querySelector("#filter-availability");
+// const filterOpen = searchFilterContainer.querySelector("filter-open");
+const tableCaption = document.querySelector("table caption");
+const filters = {
+    searchTerm: "",
+    isFree: filterPrice.checked,
+    // isExistant: filterAvailability.checked,
+    // isOpen: false,
+}
+
+asideReturnButton.addEventListener("click", toggleAside);
+searchInputField.addEventListener("keydown", handleInput);
+searchInputButton.addEventListener("click", handleInput);
+filterPrice.addEventListener("change", handleInput);
+// filterAvailability.addEventListener("change", handleInput);
+// filterOpen.addEventListener("change", handleInput);
+
+function toggleAside() {
+    aside.classList.toggle("hidden");
+}
+
+function displayResults() {
+    //Empties the previous results
+    searchResults.replaceChildren();
+    //Creates a table row for each result in the resultsArray and appends it to the DOM
+    toiletData.forEach(toilet => {
+        if (toilet.isVisible) {
             let tr = document.createElement('tr');
             let td = document.createElement('td');
             //location
@@ -20,13 +49,80 @@ fetch('https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/toilettes_
             //opening hours
             td.textContent = toilet.openinghours;
             tr.appendChild(td.cloneNode(true));
-            //excists
-            td.textContent = toilet.status;
-            tr.appendChild(td.cloneNode(true));
-
+    
             searchResults.appendChild(tr);
-        });
-    })
-    .catch(error => {
-        console.log(`Er ging iets mis: ${error.message}`); 
+        }
     });
+}
+
+function applyFilters(){
+    console.log(filters.searchTerm);
+    toiletData.forEach(toilet => {
+        // Disables all toilets that do not match with the users search input, if input is not empty
+        tableCaption.textContent = filters.searchTerm;
+
+        if (filters.searchTerm) {
+            if (toilet.location !== null && toilet.location.toLowerCase().includes(filters.searchTerm.toLowerCase())){
+                toilet.isVisible = true;
+            } else {
+                toilet.isVisible = false;
+            }
+        } else {
+            toilet.isVisible = true;
+        }
+
+        // Disables all paying toilets
+        if (filters.isFree) {
+            if (toilet.pricing_en !== 'Free'){
+                toilet.isVisible = false;
+            }
+        }
+    })
+
+    displayResults();
+}
+
+async function handleInput(event) {
+    switch (event.target) {
+        case searchInputField:
+        case searchInputButton:
+            if (event.target === searchInputField && event.key !== "Enter") return;
+            filters.searchTerm = searchInputField.value.trim();
+            searchInputField.value = "";
+
+            toiletData = await fetchData(urlDatasetPublicToilets, {limit: 100});
+            applyFilters();
+            break;
+        case filterPrice:
+            filters.isFree = (event.target.checked) ? true : false;
+            applyFilters();
+            break;
+        // case filterAvailability:
+        //     filters.isExistant = (event.target.checked) ? true : false;
+        //     applyFilters();
+        //     break;
+        // case filterOpen:
+            // console.log(event.target.checked);
+            // break;
+        default:
+            break;
+    }
+}
+
+async function fetchData(url, parameters) {
+    if (url === urlDatasetPublicToilets && parameters.limit) url = url.replace('{LIMIT}', encodeURIComponent(parameters.limit));
+
+	try {
+		const response = await fetch(url);
+		if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        const data = await response.json();
+
+        for (const toilet of data.results) {
+            toilet.isVisible = true;
+        }
+		return data.results.filter((toilet) => toilet.location !== null);
+	} catch (error) {
+		console.error(`Error while retrieving the data: ${error.message}`);
+		throw error; // Re-throw the error
+	}
+}
