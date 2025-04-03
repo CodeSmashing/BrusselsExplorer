44070d25e04897c7096c13f332e636d436c90261
NielsTanghe1 <57326188+NielsTanghe1@users.noreply.github.com>
2025-04-03 23:54:49 +0200

    Final update, polishing.
    - index.html:
    	• Change main header title to be in Dutch
    	• Change spelling of "Distance" option group to be in Dutch
    	• Change spelling of "Favourite" table head to be in Dutch
    	• Change return button to use a image
    	• Remove unneeded #search-results caption
    	• Add "Owner" table head to #search-results table
    
    - style.css:
    	• Change general styling and colors
    
    - /IMG:
    	• Add menu icon image
    
    - fetchData.js:
    	• Add comments for code explaination
    	• Add updateFavourites() function as the favourties were updates in too many places
    
    - README.md:
    	• Change completed all the requirements as mentioned in Canvas
        • Add installation guide
    
    Co-Authored-By: CodeSmashing <123628904+CodeSmashing@users.noreply.github.com>

--- JS/fetchData.js
+++ JS/fetchData.js
@@ -1,5 +1,6 @@
 'use stict';
 
+//Declaration of global variables
 const urlDatasetPublicToilets = 'https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/toilettes_publiques_vbx/records?limit={LIMIT}';
 const aside = document.querySelector("aside");
 const asideReturnButton = aside.querySelector(".return");
@@ -11,8 +12,8 @@ const filterPrice = searchFilterContainer.querySelector("#filter-price");
 const locateButton = document.getElementById('locate-button');
 const sortMenu = searchContainer.querySelector("#sort-menu");
 const favouritesContainer = document.querySelector('#favourites-container');
-const searchResults = document.querySelector('#search-results tbody');
-const tableCaption = document.querySelector("#search-results table caption");
+const searchResultsSection = document.querySelector('#search-results');
+const searchResultsTBody = searchResultsSection.querySelector('tbody');
 const barChartResults = document.getElementById('distance-bar-chart-container');
 const filters = {
     searchTerm: "",
@@ -22,33 +23,28 @@ let userData = {};
 let toiletData = [];
 let timeoutId = null;
 
+//Check if there is local data available, if not, create new data
 if (!localStorage.getItem('userData')) {
     userData = {
         currentLocation: {
             lat: 0,
             long: 0
         },
-        favourites: [
-            // {
-            //     id: "",
-            //     textContent: "",
-            // }
-        ],
+        favourites: [],
         language: '',
-        theme: ''
+        theme: 'dark'
     };
-
     localStorage.setItem('userData', JSON.stringify(userData));
     console.log('Using new data');
 } else {
     userData = JSON.parse(localStorage.getItem('userData'));
-    favouritesContainer.replaceChildren(favouritesContainer.querySelector("h2"));
-    userData.favourites.forEach(f => {
-        createFavCard(f);
-    })
+    userData.favourites.forEach(toilet => {
+        updateFavourites(toilet)
+    });
     console.log('Using existing data');
 }
 
+//Add eventlisteners to all inputs on the site
 locateButton.addEventListener("click", handleInput);
 sortMenu.addEventListener("change", handleInput);
 asideReturnButton.addEventListener("click", toggleAside);
@@ -56,14 +52,34 @@ searchInputField.addEventListener("keydown", handleInput);
 searchInputButton.addEventListener("click", handleInput);
 filterPrice.addEventListener("change", handleInput);
 
+//Hides/shows the side menu
 function toggleAside() {
     aside.classList.toggle("hidden");
+    asideReturnButton.classList.toggle("hidden");
 }
 
+//Updates the favorites array in the localstorage
+function updateFavourites(toilet, favBtn = null) {
+    if (!favBtn) favBtn = searchResultsTBody.querySelector(`button[data-for='favourite-location-card-${toilet.id}']`);
+    const oldCard = favouritesContainer.querySelector(`#favourite-location-card-${toilet.id}`);
+
+    if (Object.entries(toilet.card).length === 0) {
+        if (!oldCard) {
+            toilet.card = createFavCard(toilet);
+            favouritesContainer.appendChild(toilet.card);
+        } else {
+            toilet.card = oldCard;
+        }
+    } else {
+        if (!oldCard) favouritesContainer.appendChild(toilet.card);
+    }
+    if (favBtn) favBtn.classList.add("favourited");
+}
+
+//Displays the filtered results in a table
 function displayResults() {
     // Empties the previous results
-    searchResults.replaceChildren();
-    favouritesContainer.replaceChildren(favouritesContainer.querySelector("h2"));
+    searchResultsTBody.replaceChildren();
 
     // Creates a table row for each result in the resultsArray and appends it to the DOM
     toiletData.forEach(toilet => {
@@ -71,48 +87,55 @@ function displayResults() {
             let tr = document.createElement('tr');
             let td = document.createElement('td');
 
-            //location
+            // location
             td.textContent = toilet.location;
             tr.appendChild(td.cloneNode(true));
 
-            //pricing
+            // pricing
             td.textContent = toilet.pricing_en;
             tr.appendChild(td.cloneNode(true));
 
-            //opening hours
+            // opening hours
             td.textContent = toilet.openinghours;
             tr.appendChild(td.cloneNode(true));
 
-            //distance
+            // owner
+            td.textContent = toilet.management_en;
+            tr.appendChild(td.cloneNode(true));
+
+            // distance
             td.textContent = toilet.distance ? `${toilet.distance}m`: "No user location given";
             tr.appendChild(td.cloneNode(true));
 
             //favourite button
-            const favBtn = document.createElement("button");    
-            favBtn.textContent = "Favourite";
-            favBtn.className = 'favourite-button';
-            favBtn.dataset.for = `favourite-location-card-${toilet.id}`;
-
-            if (userData.favourites.includes(toilet.id)) {
-                favouritesContainer.appendChild(toilet.card);
-                favBtn.classList.add("favourited");
-            }
-
-            setFavButton(favBtn);
-
+            const favBtn = createFavButton(toilet.id);
             td = td.cloneNode(true);
             td.textContent = "";
             td.appendChild(favBtn);
             tr.appendChild(td);
-            searchResults.appendChild(tr);
+            searchResultsTBody.appendChild(tr);
+
+            if (userData.favourites.find((favourite) => favourite.id === toilet.id)) {
+                updateFavourites(toilet, favBtn);
+            }
         }
     });
 }
 
 function diplayBarCharts(){
+    //Makes that only the bar chart is shown
+    searchFilterContainer.style.display = 'none';
+    barChartResults.style.display = 'block';
+    searchResultsSection.style.display = 'none';
+
+    //Find the max distance of a toilet so we can scale the bars properly
+    let maxDistance = Math.max.apply(Math, toiletData.map(function(o) { return o.distance;}));
+    let cellCount = maxDistance/100;
+    
+    //Creates a table row where only a part of the cells are visible based on the value of the distance
     toiletData.forEach(toilet => {
         let toiletTable = document.createElement('table');
-        toiletTable.id = `${toilet.location}-table`;
+        toiletTable.id = `${toilet.id}-table`;
         toiletTable.className = 'distance-bar-chart';
 
         let caption = document.createElement('caption');
@@ -122,11 +145,10 @@ function diplayBarCharts(){
         let toiletTableRow = document.createElement('tr');
         toiletTable.appendChild(toiletTableRow);
         
-        //creates 100 empty cells to color in
-        for(let i = 0; i<100; i++){
+        // Creates empty cells to color in
+        for (let i = 0; i < cellCount; i++){
             let td = document.createElement('td');
             td.id = `${toilet.id}-cell-${i}`;
-            // console.log(td.id);
             td.className = 'chart-cell';
             toiletTableRow.appendChild(td);
         }
@@ -136,60 +158,75 @@ function diplayBarCharts(){
         let length = parseInt(toilet.distance/100);
         console.log(length);
 
-        for(let i = 0; i < length; i++){
+        for (let i = 0; i < length; i++){
             let currentCell = document.getElementById(`${toilet.id}-cell-${i}`);
             currentCell.className = 'filled-chart-cell';
         }
     })
 }
 
+
+//Adds a favorite card to the dom
 function createFavCard(toilet) {
-    const card = favouritesContainer.appendChild(document.createElement('div'));
+    const card = document.createElement('div');
     card.className = 'favourite-item';
     card.id = `favourite-location-card-${toilet.id}`;
-    
-    const removeBtn = card.appendChild(document.createElement("button"));
-    removeBtn.textContent = "Remove";
+
+    const cardTitle = document.createElement("p");
+    const removeBtn = document.createElement("button");
+
+    cardTitle.innerHTML = toilet.location;
+    removeBtn.textContent = "Verwijder";
     removeBtn.className = 'remove-button';
+
     removeBtn.addEventListener("click", () => {
-        favouritesContainer.removeChild(card);
-        userData.favourites.splice(userData.favourites.indexOf(toilet.id), 1);
-        console.log(searchResults.querySelector(`button[data-for='favourite-location-card-${toilet.id}]`));
-        // favourite-location-card-
+        const btn = searchResultsTBody.querySelector(`button[data-for='favourite-location-card-${toilet.id}']`);
         
-        // btn.classList.remove("favourited");
+        favouritesContainer.removeChild(card);
+        userData.favourites.splice(userData.favourites.indexOf(toilet), 1);
+        if (btn) btn.classList.remove("favourited");
+        localStorage.setItem('userData', JSON.stringify(userData));
     });
 
-    const paragraph = card.appendChild(document.createElement("p"));
-    paragraph.textContent = toilet.location;
+    card.appendChild(cardTitle);
+    card.appendChild(removeBtn);
+
     return card;
 }
 
-function setFavButton(btn) {
+//Adds the favorite button to each entry in the search result and makes sure they are displayed correctly if they are already favorited
+function createFavButton(id) {
+    const btn = document.createElement("button");    
+    btn.textContent = "Favourite";
+    btn.className = 'favourite-button';
+    btn.dataset.for = `favourite-location-card-${id}`;
     btn.addEventListener('click', () => {
         const toiletId = btn.dataset.for.replace("favourite-location-card-", "");
-        const card = toiletData.find((toilet) => toilet.id === toiletId).card;
-
-        if (userData.favourites.find((id) => id === toiletId)) { // Remove
-            userData.favourites.splice(userData.favourites.indexOf(toiletId), 1);
+        const toilet = toiletData.find((toilet) => toilet.id === toiletId);
+        
+        if (!toilet.card) return;
+        console.log("our toilet variable: ", toilet);
+        
+        if (userData.favourites.find((favourite) => favourite.id === toilet.id)) { // Remove
+            userData.favourites.splice(userData.favourites.indexOf(toilet), 1);
             btn.classList.remove("favourited");
-            favouritesContainer.removeChild(card);
+            favouritesContainer.removeChild(toilet.card);
         } else { // Add
-            userData.favourites.push(toiletId);
+            userData.favourites.push(toilet);
             btn.classList.add("favourited");
-            favouritesContainer.appendChild(card);
+            favouritesContainer.appendChild(toilet.card);
         }
 
         localStorage.setItem('userData', JSON.stringify(userData));
     })
+    return btn;
 }
 
+//Applies filters on our data fetched from the api
 function applyFilters() {
     console.info(filters.searchTerm);
     toiletData.forEach(toilet => {
         // Disables all toilets that do not match with the users search input, if input is not empty
-        tableCaption.textContent = filters.searchTerm;
-
         if (filters.searchTerm) {
             if (toilet.location !== null && toilet.location.toLowerCase().includes(filters.searchTerm.toLowerCase())) {
                 toilet.isVisible = true;
@@ -210,7 +247,7 @@ function applyFilters() {
 
     displayResults();
 }
-
+//Calculates the distance of every toilet and puts them into an array sorted from low to high
 function calculateDistance(){
     let userLocation = JSON.parse(localStorage.getItem('userData')).currentLocation;
     toiletData.forEach(toilet => {
@@ -223,7 +260,8 @@ function calculateDistance(){
     diplayBarCharts();
 }
 
-function measure(lat1, lon1, lat2, lon2){  // generally used geo measurement function
+//Calculates the distance between 2 coordinates (in meters)
+function measure(lat1, lon1, lat2, lon2){
     var R = 6378.137; // Radius of earth in KM
     var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
     var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
@@ -235,6 +273,7 @@ function measure(lat1, lon1, lat2, lon2){  // generally used geo measurement fun
     return d * 1000; // meters
 }
 
+//Gets the users location, if user denies it tells them what to do
 function getUserLocation() {
     navigator.geolocation.getCurrentPosition(position => {
         let lat = position.coords.latitude;
@@ -245,12 +284,13 @@ function getUserLocation() {
         localStorage.setItem('userData', JSON.stringify(userData));
     
         console.info('Location fetched');
+        
         calculateDistance();
-        displayResults();
+        displayResults();   
     }, error => {
         console.error('Location acces denied', error.message);
-        locateButton.textContent = "Location access denied, enable location and try again.";
-
+        locateButton.textContent = "Geen locatietoegang, geef toegang to locatie en probeer opnieuw.";
+        
         timeoutId = setTimeout(() => {
             locateButton.textContent = "Zoek WC's in mijn buurt";
             timeoutId = null;
@@ -262,9 +302,10 @@ function getUserLocation() {
     });
 }
 
+//Handles all the input
 async function handleInput(event) {
     console.log("Our handleInput event: ", event);
-    
+
     switch (event.target) {
         case searchInputField:
         case searchInputButton:
@@ -274,6 +315,9 @@ async function handleInput(event) {
 
             toiletData = await fetchData(urlDatasetPublicToilets, { limit: 100 });
             applyFilters();
+            barChartResults.style.display = 'none';
+            searchResultsSection.style.display = 'block';
+            searchFilterContainer.style.display = 'block';
             break;
         case filterPrice:
             filters.isFree = (event.target.checked) ? true : false;
@@ -310,6 +354,7 @@ async function handleInput(event) {
     }
 }
 
+//Fetch data from the API
 async function fetchData(url, parameters) {
     if (url === urlDatasetPublicToilets && parameters.limit) url = url.replace('{LIMIT}', encodeURIComponent(parameters.limit));
 
@@ -317,13 +362,16 @@ async function fetchData(url, parameters) {
         const response = await fetch(url);
         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
         const data = await response.json();
+        const shortURL = url.replace(/https:\/\/opendata\.brussels\.be\/api\/explore\/v2\.1\/catalog\/datasets\/(.*)\/.*/, "datasets.$1");
 
-        for (let i = 0; i < data.results.length; i++) {
-            data.results[i].id = `toilet-${i}`;
-            data.results[i].isVisible = true;
-            data.results[i].distance = null;
-            data.results[i].card = createFavCard(data.results[i]);
+        for (const toilet of data.results) {
+            toilet.id = `${shortURL}-toilet-${toilet.objectid}`;
+            toilet.isVisible = true;
+            toilet.distance = null;
+            toilet.origin = shortURL;
+            toilet.card = createFavCard(toilet);
         }
+        //Removes unusable data from the results
         return data.results.filter((toilet) => toilet.location !== null);
     } catch (error) {
         console.error(`Error while retrieving the data: ${error.message}`);
--- README.md
+++ README.md
@@ -38,19 +38,19 @@ Website maken waar je toiletten in Brussel kan vinden, sorteren, opslaan...
 >> **Voorbeeld uit code:** Zoekfunctie en sorteren van resultaten.
 >
 >   - Arrow functions
->> **Voorbeeld uit code:**
+>> **Voorbeeld uit code:** Functies in eventlisteners te maken
 >
 >   - Conditional (ternary) operator (moderne if..else)
->> **Voorbeeld uit code:**
+>> **Voorbeeld uit code:** Om tussen states te switchen in objects (Gratis of niet, visible of niet...)
 >
 >   - Callback functions
->> **Voorbeeld uit code:**
+>> **Voorbeeld uit code:** /
 >
 >   - Promises
->> **Voorbeeld uit code:**
+>> **Voorbeeld uit code:** Bij de het fetchen van de API data
 >
 >   - Async & Await
->> **Voorbeeld uit code:**
+>> **Voorbeeld uit code:** Om pas dingen to doen met de data, zodra deze geladen is
 >
 >   - Observer API (1 is voldoende)
 >> **Voorbeeld uit code:**
@@ -59,17 +59,17 @@ Website maken waar je toiletten in Brussel kan vinden, sorteren, opslaan...
 >**Data & API:** 
 >
 >   - Fetch om data op te halen
->> **Voorbeeld uit code:**
+>> **Voorbeeld uit code:** Brussel API
 >
 >   - JSON manipuleren en weergeven
->> **Voorbeeld uit code:**
+>> **Voorbeeld uit code:** Favorites en userdata bijhouden, opslagen, bijwerken en uitlezen 
 >
 >---
 >**Opslag & validatie:**  
 >   - Formulier validatie
->> **Voorbeeld uit code:**
+>> **Voorbeeld uit code:** Invalid input opvangen in search. Als locatietoegang geweigerd word, toegang to sommige dingen beperken om errors te voorkomen.
 >   - Gebruik van LocalStorage
->> **Voorbeeld uit code:**
+>> **Voorbeeld uit code:** Favorieten, locatie worden allemaal bijgehouden in een object in de localstorage.
 >
 >---
 >**Styling & layout:**
@@ -79,13 +79,13 @@ Website maken waar je toiletten in Brussel kan vinden, sorteren, opslaan...
 
 >## Installatie handleiding
 >   1. Dowload zip file van deze github repository
->       ![Stap 1](README_IMGS/stap1.png)
+>   ![Stap 1](README_IMGS/stap1.png)
 >   2. Extract de zip file in een folder en open deze via VS code
->       ![Stap 2](README_IMGS/stap2.png)
+>   ![Stap 2](README_IMGS/stap2.png)
 >   3. Installeer de live-server plugin in VS code
->       ![Stap 2](README_IMGS/stap3.png)
+>   ![Stap 2](README_IMGS/stap3.png)
 >   4. Start de live-server onderaan je VS code-venster
->       ![Stap 2](README_IMGS/stap4.png)
+>   ![Stap 2](README_IMGS/stap4.png)
 
 >## Gebruikte bronnen
 >   1. https://opendata.brussels.be
